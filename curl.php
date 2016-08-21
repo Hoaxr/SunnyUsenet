6cc1e20d696ecf99af9a2ae82fca605413fc33cf
Hoaxr <barthezz14@hotmail.com>
2016-08-21 23:46:45 +0200

    SunnyUsenet
    Sunnyusenet.com account creator

---
+++ curl.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Free Unlimited Newsgroup Account Generator</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Account Generator</a>
+            </div>
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Free Unlimited Newsgroup Account Generator</h1>
+                <p class="lead"><?php
+
+$length = 10;
+$randomString = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz"), 0, $length);
+$date = date('d-m-Y');
+$newDate = date("d.m.Y",strtotime($date."+7 day"));
+
+post_data("https://members.sunnyusenet.com/nl/signup/form?package=unl","firstName=sadsad&lastName=dasdas&street=dasdas&nr=11&zip=1111aa&city=dasdas&country=AL&email=" . $randomString  . "%40mailinator.com&password=A12345a&password_again=A12345a&signupForm_package=Sunny+UNL&package_id=35&signupForm_payment=0&payment_method=cc&ideal_issuer_id=&confirm_terms=&confirm_terms=1");
+echo "<br />";
+echo "<br />";
+echo "<strong>Unlimited Speed - Account Created!</strong><br />";
+echo "Expiration date: ";
+echo $newDate;
+echo "<br />";
+echo "Server address: news.sunnyusenet.com<br />";
+echo "Ports: 119, 80, 563(SSL), 443(SSL)<br />";
+echo "Connections: 20<br />";
+echo "Username: ";
+echo $randomString;
+echo "@mailinator.com<br />";
+echo "Password: A12345a";
+//echo grab_page("https://members.sunnyusenet.com/nl/signup/form?package=unl");
+
+//Upload a blank cookie.txt to the same directory as this file with a CHMOD/Permission to 777
+function login($url,$data){
+    $fp = fopen("cookie.txt", "w");
+    fclose($fp);
+    $login = curl_init();
+    curl_setopt($login, CURLOPT_COOKIEJAR, "cookie.txt");
+    curl_setopt($login, CURLOPT_COOKIEFILE, "cookie.txt");
+    curl_setopt($login, CURLOPT_TIMEOUT, 40000);
+    curl_setopt($login, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($login, CURLOPT_URL, $url);
+    curl_setopt($login, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($login, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($login, CURLOPT_POST, TRUE);
+    curl_setopt($login, CURLOPT_POSTFIELDS, $data);
+    ob_start();
+    return curl_exec ($login);
+    ob_end_clean();
+    curl_close ($login);
+    unset($login);
+}
+function grab_page($site){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 40);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+    curl_setopt($ch, CURLOPT_URL, $site);
+    ob_start();
+    return curl_exec ($ch);
+    ob_end_clean();
+    curl_close ($ch);
+}
+
+function post_data($site,$data){
+    $datapost = curl_init();
+    $headers = array("Expect:");
+    curl_setopt($datapost, CURLOPT_URL, $site);
+    curl_setopt($datapost, CURLOPT_TIMEOUT, 40000);
+    curl_setopt($datapost, CURLOPT_HEADER, TRUE);
+    curl_setopt($datapost, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($datapost, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($datapost, CURLOPT_POST, TRUE);
+    curl_setopt($datapost, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($datapost, CURLOPT_COOKIEFILE, "cookie.txt");
+    ob_start();
+    return curl_exec ($datapost);
+    ob_end_clean();
+    curl_close ($datapost);
+    unset($datapost);
+}
+
+?>
+</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
